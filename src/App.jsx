77556a6bc3dd07d8d279ec4caf725f76fd77e39e
ysinghal310 <gangbuster74@gmail.com>
2023-10-22 19:23:10 +0530

    updated tag

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,7 @@ function App() {
     <div className="min-h-screen overflow-x-hidden bg-[#F1F1F1] antialiased">
       <div className="container mx-auto">
         <h2 className="text-4xl text-center font-semibold">
-          Deployed on aws amplify
-      
+          Hello Guest, Welcome to our site!
         </h2>
         <Payroll />
       </div>
