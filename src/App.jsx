83527842d70e4f929c3c82d813779b760c31cfc3
ysinghal310 <gangbuster74@gmail.com>
2023-10-22 15:02:22 +0530

    deployed on ec2

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ function App() {
   return (
     <div className="min-h-screen overflow-x-hidden bg-[#F1F1F1] antialiased">
       <div className="container mx-auto">
+        <h2 className="text-4xl text-center font-semibold">
+          This is Sample App on dev
+        </h2>
         <Payroll />
       </div>
     </div>
